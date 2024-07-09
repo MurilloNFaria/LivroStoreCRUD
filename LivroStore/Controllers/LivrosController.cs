348c255c038fbb7eea71940ebb3ed9c2f6e87f53
Murillo Faria <nascimentomurillo24@gmail.com>
2024-07-09 16:49:18 -0300

    Criação do controlador LivrosController, criação das Views do controlador Livros, alteração do nome de controlador padrão de Home para Livros

---
+++ LivroStore/Controllers/LivrosController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LivroStore.Data;
+using LivroStore.Models;
+using AutoMapper;
+using LivroStore.Dtos;
+
+namespace LivroStore.Controllers
+{
+    public class LivrosController : Controller
+    {
+        private readonly LivroContext _context;
+        private IMapper _mapper { get; set; }
+
+        public LivrosController(LivroContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET: Livros
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Livros.ToListAsync());
+        }
+
+        // GET: Livros/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var livro = await _context.Livros
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (livro == null)
+            {
+                return NotFound();
+            }
+
+            return View(livro);
+        }
+
+        // GET: Livros/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Livros/Create
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Nome,Autor,Preco")] CreateLivroDto livroDto)
+        {
+            if (ModelState.IsValid)
+            {
+                Livro livro = _mapper.Map<Livro>(livroDto);
+                _context.Add(livro);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            // Retorna os erros do modelstate no console, caso haja algum
+            var erros = ModelState.Values.SelectMany(v => v.Errors);
+            foreach (var erro in erros)
+            {
+                Console.WriteLine(erro.ErrorMessage);
+            }
+
+            return View(livroDto);
+        }
+
+        // GET: Livros/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var livro = await _context.Livros.FindAsync(id);
+            if (livro == null)
+            {
+                return NotFound();
+            }
+            return View(livro);
+        }
+
+        // POST: Livros/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Autor,Preco")] Livro livro)
+        {
+            if (id != livro.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(livro);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LivroExists(livro.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(livro);
+        }
+
+        // GET: Livros/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var livro = await _context.Livros
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (livro == null)
+            {
+                return NotFound();
+            }
+
+            return View(livro);
+        }
+
+        // POST: Livros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var livro = await _context.Livros.FindAsync(id);
+            if (livro != null)
+            {
+                _context.Livros.Remove(livro);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LivroExists(int id)
+        {
+            return _context.Livros.Any(e => e.Id == id);
+        }
+    }
+}
