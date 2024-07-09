4b506d4d80ce42aae36c264a4b68644cfb9de316
Murillo Faria <nascimentomurillo24@gmail.com>
2024-07-09 16:26:42 -0300

    Criação da classe LivroContext e instalação da biblioteca PomeloMySql

---
+++ LivroStore/Data/LivroContext.cs
@@ -0,0 +1,14 @@
+﻿using LivroStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LivroStore.Data;
+
+public class LivroContext : DbContext
+{
+    public DbSet<Livro> Livros { get; set; }
+
+    public LivroContext(DbContextOptions<LivroContext> opts) : base(opts)
+    {
+        
+    }
+}
